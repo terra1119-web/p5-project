136eab23efb1ca759d830786ceb2e7487da04430
terra1119-web <terra1119@gmail.com>
2025-02-06 17:19:28 +0900

    リファクタリング

--- src/sketch/sketch-draw.ts
+++ src/sketch/sketch-draw.ts
@@ -2,14 +2,25 @@
 import Sketch from '@/class/Sketch'
 import p5 from 'p5'
 
+interface ParticleOptions {
+	point?: p5.Vector
+	velocity?: p5.Vector
+	acceleration?: p5.Vector
+	r?: number
+	dp?: number
+	angMult?: number
+	color?: string
+	flg?: boolean
+}
+
 class Particle {
 	p: p5
 	mainGraphics: p5.Graphics
 	overAllTexture: p5.Graphics
 	colors: string[]
 	point: p5.Vector
-	v: p5.Vector
-	a: p5.Vector
+	velocity: p5.Vector
+	acceleration: p5.Vector
 	r: number
 	dp: number
 	angMult: number
@@ -17,7 +28,7 @@ class Particle {
 	flg: boolean
 
 	constructor(
-		args: object,
+		args: ParticleOptions,
 		p: p5,
 		mainGraphics: p5.Graphics,
 		overAllTexture: p5.Graphics
@@ -25,22 +36,36 @@ class Particle {
 		this.p = p
 		this.mainGraphics = mainGraphics
 		this.overAllTexture = overAllTexture
-		this.colors =
-			'e6e1c5-d4cb92-395c6b-80a4ed-bcd3f2-f24-fff-52489c-4062bb-59c3c3-ebebeb-f45b69-0c090d-e01a4f-f15946-f9c22e-53b3cb'
-				.split('-')
-				.map(a => '#' + a)
-		const def: object = {
+		this.colors = [
+			'#e6e1c5',
+			'#d4cb92',
+			'#395c6b',
+			'#80a4ed',
+			'#bcd3f2',
+			'#f24',
+			'#fff',
+			'#52489c',
+			'#4062bb',
+			'#59c3c3',
+			'#ebebeb',
+			'#f45b69',
+			'#0c090d',
+			'#e01a4f',
+			'#f15946',
+			'#f9c22e',
+			'#53b3cb'
+		]
+		const def: ParticleOptions = {
 			point: this.p.createVector(0, 0),
-			v: this.p.createVector(0, 0),
-			a: this.p.createVector(0, 0),
+			velocity: this.p.createVector(0, 0),
+			acceleration: this.p.createVector(0, 0),
 			r: 10,
 			dp: this.p.random(0.93, 0.99),
 			angMult: this.p.random(10, 50),
 			color: this.p.random(this.colors),
 			flg: false
 		}
-		Object.assign(def, args)
-		Object.assign(this, def)
+		Object.assign(this, { ...def, ...args })
 	}
 
 	draw(): void {
@@ -56,9 +81,7 @@ class Particle {
 		this.mainGraphics.pop()
 	}
 
-	update(): void {
-		this.point.add(this.v)
-		this.v.add(this.a)
+	updateVelocity(): void {
 		let delta: p5.Vector = this.p.createVector(
 			this.point.x - this.p.width / 2,
 			this.point.y - this.p.height / 2
@@ -66,17 +89,22 @@ class Particle {
 		let ang: number = delta.heading()
 		let rr: number = delta.mag()
 
-		this.v.x +=
+		this.velocity.x +=
 			-this.p.sin(ang * this.angMult + rr / 5) / 15 +
 			this.p.cos(rr / 10) / 10
-		this.v.y +=
+		this.velocity.y +=
 			-this.p.cos(ang * this.angMult + rr / 5) / 15 +
 			this.p.sin(rr / 10) / 10
-		this.a.x = (this.p.noise(this.point.x, this.point.y, 5) - 0.5) * 1.1
-		this.a.y = (this.p.noise(this.point.x, this.point.y, 5000) - 0.5) * 1.1
+	}
 
-		this.v.mult(0.95)
-		// this.r*= this.dp
+	updateAcceleration(): void {
+		this.acceleration.x =
+			(this.p.noise(this.point.x, this.point.y, 5) - 0.5) * 1.1
+		this.acceleration.y =
+			(this.p.noise(this.point.x, this.point.y, 5000) - 0.5) * 1.1
+	}
+
+	updateRadius(): void {
 		if (this.r > 120 || this.r < 10) this.flg = !this.flg
 		if (this.flg) {
 			this.r *= this.dp
@@ -84,20 +112,25 @@ class Particle {
 			this.r += this.dp * 0.9
 		}
 	}
+
+	update(): void {
+		this.point.add(this.velocity)
+		this.velocity.add(this.acceleration)
+		this.updateVelocity()
+		this.updateAcceleration()
+		this.velocity.mult(0.95)
+		this.updateRadius()
+	}
 }
 
 class SketchTest extends Sketch {
-	particles: Particle[]
+	particles: Particle[] = []
 	mainGraphics: p5.Graphics
 	overAllTexture: p5.Graphics
 	p: p5
 
 	constructor() {
 		super({})
-		// variables
-		this.particles = []
-		this.mainGraphics
-		this.overAllTexture
 	}
 
 	setup(): void {
@@ -105,9 +138,15 @@ class SketchTest extends Sketch {
 		this.overAllTexture = this.p.createGraphics(this.p.width, this.p.height)
 		this.mainGraphics = this.p.createGraphics(this.p.width, this.p.height)
 		// mainGraphics.blendMode(s.ADD)
-		this.overAllTexture.loadPixels()
+		this.createOverAllTexture()
 
-		// noStroke()
+		this.p.background(0)
+		this.createParticles()
+		this.mainGraphics.background(0)
+	}
+
+	createOverAllTexture(): void {
+		this.overAllTexture.loadPixels()
 		for (let i: number = 0; i < this.p.width + 50; i++) {
 			for (let o: number = 0; o < this.p.height + 50; o++) {
 				this.overAllTexture.set(
@@ -122,15 +161,16 @@ class SketchTest extends Sketch {
 			}
 		}
 		this.overAllTexture.updatePixels()
+	}
 
-		this.p.background(0)
+	createParticles(): void {
 		for (let i: number = 0; i < this.p.width; i += 200) {
 			for (let o: number = 0; o < this.p.height; o += 100) {
 				this.particles.push(
 					new Particle(
 						{
 							point: this.p.createVector(i, o),
-							v: this.p.createVector(
+							velocity: this.p.createVector(
 								this.p.noise(i / 10) * 10 - 5,
 								this.p.noise(o / 10) * 10 - 5
 							),
@@ -143,7 +183,6 @@ class SketchTest extends Sketch {
 				)
 			}
 		}
-		this.mainGraphics.background(0)
 	}
 
 	draw(): void {
